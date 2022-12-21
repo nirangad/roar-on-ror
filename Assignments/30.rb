e3694d89716b8e7b26870594a1f21cf53ba2f98f
Niranga De Silva <nirangad@gmail.com>
2022-12-21 23:05:16 +0530

    30. Assignment (Data modified)

--- Assignments/30.rb
+++ Assignments/30.rb
@@ -1,11 +1,18 @@
 # frozen_string_literal: true
 
-users = {
-  "user_1": 'abcd12341',
-  "user_2": 'abcd12342',
-  "user_3": 'abcd12343',
-  "user_4": 'abcd12344'
-}
+# users = {
+#   "user_1": 'abcd12341',
+#   "user_2": 'abcd12342',
+#   "user_3": 'abcd12343',
+#   "user_4": 'abcd12344'
+# }
+
+users = [
+  { username: 'user_1', password: 'abcd12341' },
+  { username: 'user_2', password: 'abcd12342' },
+  { username: 'user_3', password: 'abcd12343' },
+  { username: 'user_4', password: 'abcd12344' }
+]
 
 puts 'Welcome to the Authenticator'
 puts '-' * 30
@@ -21,12 +28,14 @@ until stop
   puts 'Enter Password: '
   pass = gets.chomp
 
-  if users[name.to_s.to_sym] == pass
-    user = {
+  user = users.find { |u| u[:username] == name && u[:password] == pass }
+
+  if !user.nil?
+    u = {
       username: name,
-      password: users[name.to_s.to_sym]
+      password: pass
     }
-    puts user
+    puts u
   else
     puts 'Invalid credentials'
   end
